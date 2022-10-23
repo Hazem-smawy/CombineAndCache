f8f9c3afd3e895f9c19134a800658962322c3a52
hazem-smawy <hazemsmawy@gmail.com>
2022-10-23 17:04:15 +0300

    Initial Commit

---
+++ CombineAndCache/CombineAndCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAndCacheApp.swift
+//  CombineAndCache
+//
+//  Created by hazem smawy on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineAndCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineAndCache/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineAndCache
+//
+//  Created by hazem smawy on 10/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
